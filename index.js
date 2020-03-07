72d0130359d011a8f1acdc782cc4f4d33d73f3b1
raspyweather <raspyweather@gmail.com>
2020-03-07 16:29:02 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+if (String.prototype["padStart"] === undefined) {
+    String.prototype["padStart"] = function (str, char, count) {
+        return (str.length >= count) ? str : Array(1 + count - str.length).join(char) + str;
+    };
+}
+var dotW = 10;
+var dotH = 10;
+var PatternPainter = /** @class */ (function () {
+    function PatternPainter(context) {
+        this.context = context;
+    }
+    PatternPainter.prototype.draw = function (draw, xOffset, yOffset) {
+        var _this = this;
+        if (xOffset === void 0) { xOffset = 0; }
+        if (yOffset === void 0) { yOffset = 0; }
+        var drawDot = function (x, y, color) {
+            _this.context.fillStyle = color;
+            _this.context.fillRect(x * dotW + xOffset, y * dotH + yOffset, dotW, dotH);
+        };
+        var pattern = draw.pattern.map(function (row) { return row.map(function (item) { return draw.colors[item]; }); });
+        var w = window.innerWidth, h = window.innerWidth;
+        var dotWCount = w / dotW, dotHCount = h / dotH;
+        var x = 0;
+        var intervalId = setInterval(function () {
+            if (x > dotWCount) {
+                clearInterval(intervalId);
+                return;
+            }
+            pattern.forEach(function (row, cy) { return row.forEach(function (patternPx, cx) {
+                return drawDot(x + cx, cy, "#" + patternPx.toString(16).padStart(6, 0));
+            }); });
+            x += draw.pattern[0].length;
+        }, 0);
+    };
+    PatternPainter.prototype.drawPage = function (pageConfig) {
+        var _this = this;
+        var drawDir = function (configs, yStart) {
+            var dotYIdx = 0;
+            for (var index = 0; index < configs.length; index++) {
+                var element = configs[index];
+                _this.draw(element, 0, yStart + dotH * dotYIdx);
+                dotYIdx += element.pattern.length + 1;
+            }
+            return dotYIdx;
+        };
+        var y = drawDir(pageConfig.configs, 0);
+        if (pageConfig.symmetry) {
+            drawDir(pageConfig.configs.reverse().map(function (row) { row.pattern = row.pattern.reverse(); return row; }), window.innerHeight - y * dotH);
+        }
+    };
+    return PatternPainter;
+}());
---
+++ index.ts
@@ -0,0 +1,57 @@
+interface DrawingConfig {
+    pattern: number[][];
+    colors: number[];
+}
+
+interface PageConfig {
+    version:string;
+    text: string;
+    configs: DrawingConfig[];
+    symmetry: boolean;
+}
+
+if (String.prototype["padStart"] === undefined) {
+    String.prototype["padStart"] = (str, char, count) =>
+        (str.length >= count) ? str : Array(1 + count - str.length).join(char) + str;
+}
+
+const dotW = 10;
+const dotH = 10;
+
+class PatternPainter {
+    constructor(private readonly context: CanvasRenderingContext2D) { }
+    draw(draw: DrawingConfig, xOffset = 0, yOffset = 0) {
+        const drawDot = (x, y, color) => {
+            this.context.fillStyle = color;
+            this.context.fillRect(x * dotW + xOffset, y * dotH + yOffset, dotW, dotH);
+        }
+        const pattern = draw.pattern.map(row => row.map(item => draw.colors[item]));
+        const w = window.innerWidth, h = window.innerWidth;
+        const dotWCount = w / dotW, dotHCount = h / dotH;
+        let x = 0;
+        let intervalId = setInterval(() => {
+            if (x > dotWCount) {
+                clearInterval(intervalId);
+                return;
+            }
+            pattern.forEach((row, cy) => row.forEach((patternPx, cx) =>
+                drawDot(x + cx, cy, "#" + patternPx.toString(16).padStart(6, 0))));
+            x += draw.pattern[0].length;
+        }, 0);
+    }
+    drawPage(pageConfig: PageConfig) {
+        const drawDir = (configs, yStart) => {
+            let dotYIdx = 0;
+            for (let index = 0; index < configs.length; index++) {
+                const element = configs[index];
+                this.draw(element, 0, yStart + dotH * dotYIdx);
+                dotYIdx += element.pattern.length + 1;
+            }
+            return dotYIdx;
+        };
+        const y = drawDir(pageConfig.configs, 0);
+        if (pageConfig.symmetry) {
+            drawDir(pageConfig.configs.reverse().map(row => { row.pattern = row.pattern.reverse(); return row; }), window.innerHeight - y * dotH);
+        }
+    }
+}
